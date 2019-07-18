7d72cec2c1f1ab07a5b916cce55d6bea18bb8632
lilu <lilu@liludeMacBook-Pro.local>
2019-07-18 17:22:33 +0800

    update

--- react-native/Libraries/Text/TextInput/RCTBaseTextInputView.m
+++ react-native/Libraries/Text/TextInput/RCTBaseTextInputView.m
@@ -311,7 +311,7 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithFrame:(CGRect)frame)
 
   _nativeEventCount++;
 
-  if (_onChange && backedTextInputView.markedTextRange == nil) {
+  if (_onChange) {
     _onChange(@{
        @"text": self.attributedText.string,
        @"target": self.reactTag,
